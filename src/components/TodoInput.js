b7e2df54631de4a3417dc2ec958f7ea6b11a4e75
Chin Kuay <kuaychin@gmail.com>
2020-03-28 18:01:09 -0500

    modified

--- src/components/TodoInput.js
+++ src/components/TodoInput.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-// hello world
 export default class TodoInput extends Component {
   render() {
     const { item, handleChange, handleSubmit, editItem } = this.props;
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-
 export default class TodoItem extends Component {
   render() {
     const { title, handleDelete, handleEdit } = this.props;
