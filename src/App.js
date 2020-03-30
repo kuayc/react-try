05fb6272c130476491164da02e4e967b534f14fe
Chin Kuay <kuaychin@gmail.com>
2020-03-30 02:50:49 -0500

    added functional component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import uuid from "uuid";
 import "bootstrap/dist/css/bootstrap.min.css";
 import TodoInput from "./components/TodoInput";
 import TodoList from "./components/TodoList";
+import FunctionalComponent from "./components/functionalComponent";
 class App extends Component {
   state = {
     items: [],
@@ -52,7 +53,7 @@ class App extends Component {
   };
   render() {
     return (
-      <div className="container">
+      <nav className="container">
         <div className="row">
           <div className="col-10 mx-auto col-md-8 mt-5">
             <h3 className="text-capitalize text-center">What To Do Today?</h3>
@@ -70,7 +71,10 @@ class App extends Component {
             />
           </div>
         </div>
-      </div>
+        <footer>
+          <FunctionalComponent />
+        </footer>
+      </nav>
     );
   }
 }
---
+++ src/components/functionalComponent.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function FunctionalComponent() {
+  return (
+    <div>
+      <h4>@copyright</h4>
+    </div>
+  );
+}
+
+export default FunctionalComponent;
